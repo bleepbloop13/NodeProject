6de0436bca683af65987e39c220765aef1e1f576
bleepbloop13 <rbak9218@740s-pl-161.local>
2016-01-27 13:04:16 -0700

    Added to Repo

---
+++ src/Controller/NodeController.cpp
@@ -0,0 +1,18 @@
+/*
+ * NodeController.cpp
+ *
+ *  Created on: Jan 27, 2016
+ *      Author: rbak9218
+ */
+
+#include "NodeController.h"
+
+NodeController::NodeController() {
+	// TODO Auto-generated constructor stub
+
+}
+
+NodeController::~NodeController() {
+	// TODO Auto-generated destructor stub
+}
+
