dbd5fec1a5b67f390f0d3484aab96341d30f9dc2
bleepbloop13 <rbak9218@740s-pl-161.local>
2016-02-02 12:42:46 -0700

    Added constructor

--- src/Model/CTECArray.cpp
+++ src/Model/CTECArray.cpp
@@ -7,12 +7,15 @@
 
 #include "CTECArray.h"
 
-CTECArray::CTECArray() {
-	// TODO Auto-generated constructor stub
-
+template <class Type>
+CTECArray<Type>::CTECArray(int size)
+{
+	this->size = size;
 }
 
-CTECArray::~CTECArray() {
+template <class Type>
+CTECArray<Type>::~CTECArray()
+{
 	// TODO Auto-generated destructor stub
 }
 
